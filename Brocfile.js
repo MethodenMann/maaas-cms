a34be8b698964fe1cf86a320409f44c6f5452112
MethodenMann <dkeller.ch@gmail.com>
2015-09-17 18:00:54 +0200

    support typescript

--- Brocfile.js
+++ Brocfile.js
@@ -12,6 +12,38 @@ var jade = require('broccoli-jade');
 console.log("build for " + env)
 
 
+function prepareJs() {
+  function pickTypeScripts(root) {
+    tree = pickFiles(root, {
+      srcDir: '/',
+      destDir: '/js'
+    })
+    return compileTypeScript(tree, {})
+  }
+  var app = pickTypeScripts('www_source/scripts')
+
+  var sourceTrees = [app]
+  if (env !== 'production') {
+    var appDev = pickTypeScripts('www_source/dev/js')
+    sourceTrees.push(appDev)
+  }
+  var appJs = new mergeTrees(sourceTrees, { overwrite: true })
+
+  appJs = concat(appJs, {
+    inputFiles: [
+      '**/*.js'
+    ],
+    outputFile: '/js/app.js',
+    separator: '\n',
+    wrapInFunction: true
+  })
+
+  if (env === 'production') {
+    appJs = uglifyJavaScript(appJs, {})
+  }
+  return appJs
+}
+
 
 
 
@@ -57,4 +89,4 @@ function copyRemainingAssets() {
   });
 }
 
-module.exports = mergeTrees([prepareTemplates(), prepareCss(), copyRemainingAssets()])
+module.exports = mergeTrees([prepareJs(), prepareTemplates(), prepareCss(), copyRemainingAssets()])
