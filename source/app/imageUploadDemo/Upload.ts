163920df3f6e42377ac0540388d76b57350ce59b
Samuel Mueller <mueller.samu@gmail.com>
2015-09-29 16:49:14 +0200

    fix routes

--- source/app/imageUploadDemo/Upload.ts
+++ source/app/imageUploadDemo/Upload.ts
@@ -1,12 +1,10 @@
 /// <reference path='../_all.ts' />
 module maaas {
-  maaas.app.config(['$routeProvider',
-    function routes($routeProvider) {
-      $routeProvider
-        .when('/imageUploadDemo', {
-        templateUrl: './app/imageUploadDemo/view/upload.html'
-        // controller: 'LoginCtrl'
-      });
-    }
-  ]);
+  maaas.app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('cms.imageUploadDemo', {
+      url: '/imageUploadDemo',
+      templateUrl: './app/imageUploadDemo/view/upload.html'
+    });
+  });
 }
