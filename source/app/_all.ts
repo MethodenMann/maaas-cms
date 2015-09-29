1779a6eb3d1d86f661a571c7b71f07b086f86d05
MethodenMann <dkeller.ch@gmail.com>
2015-09-29 16:36:47 +0200

    change angular router and implement basic nested views

--- source/app/_all.ts
+++ source/app/_all.ts
@@ -5,4 +5,5 @@
 /// <reference path='./login/LoginService.ts'/>
 /// <reference path='./login/LoginCtrl.ts'/>
 /// <reference path='./test/TestCtrl.ts'/>
+/// <reference path='./master/Master.ts'/>
 /// <reference path='./login/Login.ts'/>
