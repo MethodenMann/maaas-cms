a97c958ff9be959ea485800a1004712aef60f7b1
Samuel Mueller <mueller.samu@gmail.com>
2015-09-29 16:58:44 +0200

    fix routes

--- source/app/registration/Registration.ts
+++ source/app/registration/Registration.ts
@@ -2,17 +2,13 @@
 
 module maaas {
   maaas.app.controller('RegistrationCtrl', RegistrationCtrl);
-  // maaas.app.service('LoginService', LoginService);
-  // maaas.app.directive('loginButton', <any>LoginButton);
 
-
-  maaas.app.config(['$routeProvider',
-    function routes($routeProvider) {
-      $routeProvider
-        .when('/registration', {
-        templateUrl: './app/registration/view/registration.html',
-        controller: 'RegistrationCtrl'
-      });
-    }
-  ]);
+  maaas.app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('registration', {
+      url: '/registration',
+      templateUrl: './app/registration/view/registration.html',
+      controller: 'RegistrationCtrl'
+    });
+  });
 }
