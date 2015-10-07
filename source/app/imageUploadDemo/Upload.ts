f7c7bb6496c667a6f4c5f9c27b30e109911c02d5
Samuel Mueller <mueller.samu@gmail.com>
2015-10-07 10:00:53 +0200

    delete imageUploadDemo

--- source/app/imageUploadDemo/Upload.ts
+++
@@ -1,9 +0,0 @@
-import {app} from '../app';
-
-app.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('cms.imageUploadDemo', {
-    url: '/imageUploadDemo',
-    templateUrl: './app/imageUploadDemo/view/upload.html'
-  });
-});
