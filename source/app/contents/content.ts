9fde7d8392e804aede0d353bb30d1f826fca0e39
Samuel Mueller <mueller.samu@gmail.com>
2015-10-20 17:41:56 +0200

    cleanup

--- source/app/contents/content.ts
+++ source/app/contents/content.ts
@@ -1,16 +1,11 @@
 import {makeDirective, makeSelector} from '../utils/component';
 import {RouteUtil} from '../master/route-util';
-import {TinyMceComponent} from './tiny-mce-component';
 import {CreateView} from '../contents/create-view';
 import {UpdateView} from '../contents/update-view';
 import {ListView} from './list-view';
 
 export function loadContent(app) {
   var componentName = 'contents';
-
-  app.directive(
-    makeSelector(TinyMceComponent),
-    makeDirective(TinyMceComponent));
 
   app.directive(
     makeSelector(CreateView),
@@ -29,7 +24,6 @@ export function loadContent(app) {
       .state(`cms.areas.detail.${componentName}.create`,
         RouteUtil.getRoute(CreateView,
           'Inhalt erstellen', `cms.areas.detail.${componentName}.list`, '/create'))
-
       .state(`cms.areas.detail.${componentName}.update`,
         RouteUtil.getRoute(UpdateView,
           'Inhalt bearbeiten', `cms.areas.detail.${componentName}.list`, '/{contentId:[0-9]{1,8}}'));
