3963188ce498399354e2de589e70591db630f27f
MethodenMann <dkeller.ch@gmail.com>
2015-10-21 16:32:35 +0200

    remove dev depencencies

---
+++ source/app/challenges/kind-configs/true-false-question.ts
@@ -0,0 +1,8 @@
+import {Inject} from '../../utils/di';
+
+export class TrueFalseAnswer {
+  public static selector = 'mas-challenges-true-false-answer';
+  public static templateUrl = './app/challenges/kind-configs/true-false-answer.html';
+
+  constructor() {}
+}
