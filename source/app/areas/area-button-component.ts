12e93268cf4bff4c54193fe775cbeb419a76d053
MethodenMann <dkeller.ch@gmail.com>
2015-10-29 17:17:45 +0100

    lint error for travis testing

--- source/app/areas/area-button-component.ts
+++ source/app/areas/area-button-component.ts
@@ -3,7 +3,7 @@ import {IArea} from './IArea';
 
 export class AreaButtonComponent {
 
-  private static templateUrl = './app/areas/area-button-component.html';
+  private static templateUrl = "./app/areas/area-button-component.html";
   private static selector = 'mas-area-button';
 
   private static options = {
