a4e5babcbe425e948e280cee5ec215613bb1c6b5
Samuel Mueller <mueller.samu@gmail.com>
2015-11-03 12:00:20 +0100

    make it better

--- source/app/translation/list-view.ts
+++ source/app/translation/list-view.ts
@@ -16,23 +16,36 @@ export class ListView {
   private currentModelName:string;
   private currentModelIndex:number;
 
+  private fieldConfigs = {
+    'area': [
+      {name: 'name', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'gotoText', prefix: 'areas_details_gototext', inputType: 'input'}
+    ],
+    'content': [
+      {name: 'title', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'description', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'data', prefix: 'areas_details_gototext', inputType: 'richtext'}
+    ],
+    'challenge': [
+      {name: 'name', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'data', prefix: 'areas_details_gototext', inputType: 'quiz-multiple-choice'}
+    ]
+  }
+
+  private modelConfigs:Array<{value:string, modelType:any}>;
+  private selectedModel:{value:string, modelType:any};
+
   constructor(
     @Inject('$scope') private $scope,
     @Inject('Area') private Area,
+    @Inject('Content') private Content,
     @Inject('Challenge') private Challenge
     ) {
 
-      this.currentModelType = this.Challenge;
-      this.currentModelName = 'challenge';
-
-      this.fields.push({name: 'name', prefix: 'areas_details_name', inputType: 'input'});
-      this.fields.push({name: 'data', prefix: 'areas_details_gototext', inputType: 'quiz-multiple-choice'});
-
-      this.currentModelType.findAll({locale: this.mainLanguage, translations: "yes"}).then((models) => {
-        this.allModels = models;
-        this.currentModelIndex = 0;
-        this.currentModel = this.allModels[this.currentModelIndex];
-      })
+    this.modelConfigs = [];
+    this.modelConfigs.push({value: 'area', modelType: Area});
+    this.modelConfigs.push({value: 'content', modelType: Content});
+    this.modelConfigs.push({value: 'challenge', modelType: Challenge});
   }
 
   loadPreviousModel() {
@@ -41,7 +54,7 @@ export class ListView {
   }
 
   loadNextModel() {
-    this.currentModelIndex = Math.min(0, ++this.currentModelIndex);
+    this.currentModelIndex = Math.min(this.allModels.length - 1, ++this.currentModelIndex);
     this.loadModel();
   }
 
@@ -50,10 +63,25 @@ export class ListView {
   }
 
   save() {
-    for (let locale of this.locales) {
-      var payload = {locale: locale};
-      payload[this.currentModelName] = this.currentModel.translations[locale];
-      this.currentModelType.update(this.currentModel.id, payload);
-    }
+    var payload = {};
+    payload[this.currentModelName] = this.currentModel;
+    this.currentModelType.update(this.currentModel.id, payload);
+
+    // for (let locale of this.locales) {
+    //   var payload = {locale: locale};
+    //   payload[this.currentModelName] = this.currentModel.translations[locale];
+    // }
+  }
+
+  selectedModelChanged() {
+    this.currentModelType = this.selectedModel.modelType;
+    this.currentModelName = this.selectedModel.value;
+    this.fields = this.fieldConfigs[this.selectedModel.value];
+
+    this.currentModelType.findAll({locale: this.mainLanguage, translations: "yes"}).then((models) => {
+      this.allModels = models;
+      this.currentModelIndex = 0;
+      this.currentModel = this.allModels[this.currentModelIndex];
+    })
   }
 }
