f27e0932f4aea7626a740b370aa8fe7ef97f2034
Samuel Mueller <mueller.samu@gmail.com>
2015-11-17 15:11:40 +0100

    :art:

--- source/app/translation/translatable.ts
+++ source/app/translation/translatable.ts
@@ -3,62 +3,62 @@ export class Translatable {
   public fieldConfig:any;
 
   constructor(public modelConfig:any, public model:any) {
-    this.fieldConfig = this.fieldConfigs[modelConfig.name](model);
+    this.fieldConfig = fieldConfigs[modelConfig.name](model);
   }
 
   prepareModel() {
-    this.modelPreparators[this.modelConfig.name](this.model);
+    modelPreparators[this.modelConfig.name](this.model);
   }
-
-  private fieldConfigs = {
-    'area': () => {
-      return [
-        {name: 'name', prefix: 'areas_details_name', inputType: 'input'},
-        {name: 'gotoText', prefix: 'areas_details_gototext', inputType: 'input'}
-      ];
-    },
-    'content': () => {
-      return [
-        {name: 'title', prefix: 'areas_details_name', inputType: 'input'},
-        {name: 'description', prefix: 'areas_details_name', inputType: 'input'},
-        {name: 'data', prefix: 'areas_details_gototext', inputType: 'richtext'}
-      ];
-    },
-    'challenge': (currentModel:any) => {
-      return [
-        {name: 'name', prefix: 'challenges_details_name', inputType: 'input'},
-        {name: 'data', prefix: 'areas_details_gototext', inputType: `quiz-${currentModel.kind}`}
-      ];
-    }
-  };
-
-  private quizPreparators = {
-    'multiple-choice': (data) => {
-      var clone = jQuery.extend(true, {}, data);
-      for (let answer of clone.answers) {
-        answer.text = '';
-      }
-      return clone;
-    },
-
-    'true-false': (data) => {
-      var clone = jQuery.extend(true, {}, data);
-      for (let question of clone.questions) {
-        question.text = '';
-      }
-      return clone;
-    }
-  };
-
-  private modelPreparators = {
-    'area': () => {}, 'content': () => {},
-    'challenge': (currentModel:any) => {
-      var preparator = this.quizPreparators[currentModel.kind];
-      for (let locale of this.locales) {
-        if (jQuery.isEmptyObject(currentModel.translations[locale].data)) {
-          currentModel.translations[locale].data = preparator(currentModel.data);
-        }
-      }
-    }
-  };
 }
+
+var fieldConfigs = {
+  'area': () => {
+    return [
+      {name: 'name', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'gotoText', prefix: 'areas_details_gototext', inputType: 'input'}
+    ];
+  },
+  'content': () => {
+    return [
+      {name: 'title', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'description', prefix: 'areas_details_name', inputType: 'input'},
+      {name: 'data', prefix: 'areas_details_gototext', inputType: 'richtext'}
+    ];
+  },
+  'challenge': (currentModel:any) => {
+    return [
+      {name: 'name', prefix: 'challenges_details_name', inputType: 'input'},
+      {name: 'data', prefix: 'areas_details_gototext', inputType: `quiz-${currentModel.kind}`}
+    ];
+  }
+};
+
+var quizPreparators = {
+  'multiple-choice': (data) => {
+    var clone = jQuery.extend(true, {}, data);
+    for (let answer of clone.answers) {
+      answer.text = '';
+    }
+    return clone;
+  },
+
+  'true-false': (data) => {
+    var clone = jQuery.extend(true, {}, data);
+    for (let question of clone.questions) {
+      question.text = '';
+    }
+    return clone;
+  }
+};
+
+var modelPreparators = {
+  'area': () => {}, 'content': () => {},
+  'challenge': (currentModel:any) => {
+    var preparator = this.quizPreparators[currentModel.kind];
+    for (let locale of this.locales) {
+      if (jQuery.isEmptyObject(currentModel.translations[locale].data)) {
+        currentModel.translations[locale].data = preparator(currentModel.data);
+      }
+    }
+  }
+};
