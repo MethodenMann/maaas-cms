549a8b0af572e8e785abc848c6b9ef0f07a81371
Samuel Mueller <mueller.samu@gmail.com>
2015-11-17 17:57:53 +0100

    :art:

--- source/app/translation/list-view.ts
+++ source/app/translation/list-view.ts
@@ -19,6 +19,7 @@ export class ListView {
 
   constructor(
     @Inject('$scope') private $scope,
+    @Inject('$q') private $q,
     @Inject('Area') private Area,
     @Inject('Content') private Content,
     @Inject('Challenge') private Challenge
@@ -29,17 +30,24 @@ export class ListView {
     this.modelConfigs.push({name: 'content', modelType: Content});
     this.modelConfigs.push({name: 'challenge', modelType: Challenge});
 
+    var promises = [];
+
     for (let modelConfig of this.modelConfigs) {
       // this needs to be called in a self executing function, otherwise
       // the modelConfig variable gets overridden for other promises
       ((modelConfig) => {
-        modelConfig.modelType.findAll({locale: this.mainLanguage, translations: 'yes'}).then((models) => {
+        var promise = modelConfig.modelType.findAll({locale: this.mainLanguage, translations: 'yes'}).then((models) => {
           for (let model of models) {
             this.translatables.push(new Translatable(modelConfig, model));
           }
         });
+        promises.push(promise);
       })(modelConfig);
     }
+
+    $q.all(promises).then(() => {
+      this.updateTranslationProgress();
+    })
   }
 
   getTranslatable() {
@@ -47,20 +55,25 @@ export class ListView {
   }
 
   loadPreviousModel() {
+    this.save();
     this.currentModelIndex = Math.max(0, --this.currentModelIndex);
   }
 
   loadNextModel() {
+    this.save();
     this.currentModelIndex = Math.min(this.translatables.length - 1, ++this.currentModelIndex);
-    this.updateTranslationProgress();
   }
 
   save() {
+    this.$scope.$broadcast('mas.saveprogess', 'in-progress');
     var payload = {};
     var translatable = this.getTranslatable();
     payload[translatable.modelConfig.name] = translatable.model;
     payload['locale'] = this.mainLanguage;
-    translatable.modelConfig.modelType.update(translatable.model.id, payload);
+    translatable.modelConfig.modelType.update(translatable.model.id, payload).then(() => {
+      this.$scope.$broadcast('mas.saveprogess', 'successfully');
+    });
+    this.updateTranslationProgress();
   }
 
   updateTranslationProgress() {
