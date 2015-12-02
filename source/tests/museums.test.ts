68f9d58d9d055ecc7e65eca7b5d71986579dbc1a
Samuel Mueller <mueller.samu@gmail.com>
2015-12-02 14:37:47 +0100

    fix indentation

--- source/tests/museums.test.ts
+++ source/tests/museums.test.ts
@@ -6,56 +6,56 @@ import '../app/app'
 import '../app/template-cache';
 
 describe('Museums', () => {
- console.log('Test Museums1');
+  console.log('Test Museums1');
 
 
- let _$scope;
- let _$compile;
- let _$q;
+  let _$scope;
+  let _$compile;
+  let _$q;
 
- var MuseumServiceMock =  {
-     find: () => {
-       var deferred = _$q.defer();
-       deferred.resolve({ 'name' : 'nmsg', 'description': 'blabla'});
-       return deferred.promise;
-     }
-   };
-
-
-
- // angular.module('cms', ['maaas.templates', 'pascalprecht.translate', 'ui.router'])
- //   .directive(
- //     makeSelector(DetailUpdateView),
- //     makeDirective(DetailUpdateView));
+  var MuseumServiceMock = {
+    find: () => {
+      var deferred = _$q.defer();
+      deferred.resolve({ 'name' : 'nmsg', 'description': 'blabla'});
+      return deferred.promise;
+    }
+  };
 
 
- beforeEach(angular.mock.module('maaas'));
 
- beforeEach(() => {
-   angular.mock.module(function ($provide) {
-     $provide.value('Museum', MuseumServiceMock);
-   });
-
-
-   angular.mock.inject(($compile, $rootScope, $q) => {
-     _$scope = $rootScope.$new();
-     _$compile = $compile;
-     _$q = $q;
-   });
- });
-
- it('generate the appropriate HTML...', () => {
-   _$scope.museum = {
-     name: 'nmsg'
-   };
+  // angular.module('cms', ['maaas.templates', 'pascalprecht.translate', 'ui.router'])
+  //   .directive(
+  //     makeSelector(DetailUpdateView),
+  //     makeDirective(DetailUpdateView));
 
 
-   let element = angular.element(
-     `<mas-museums-detail-update-view/>`);
+  beforeEach(angular.mock.module('maaas'));
 
-   let compiled = _$compile(element)(_$scope);
+  beforeEach(() => {
+    angular.mock.module(function ($provide) {
+      $provide.value('Museum', MuseumServiceMock);
+    });
 
-   _$scope.$digest();
-   chai.expect(compiled.html()).to.contain('form');
- });
+
+    angular.mock.inject(($compile, $rootScope, $q) => {
+      _$scope = $rootScope.$new();
+      _$compile = $compile;
+      _$q = $q;
+    });
+  });
+
+  it('generate the appropriate HTML...', () => {
+    _$scope.museum = {
+      name: 'nmsg'
+    };
+
+
+    let element = angular.element(
+      `<mas-museums-detail-update-view/>`);
+
+    let compiled = _$compile(element)(_$scope);
+
+    _$scope.$digest();
+    chai.expect(compiled.html()).to.contain('form');
+  });
 });
